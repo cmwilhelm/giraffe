3e8f0758024594bf8f63dbc392a8bdc3f821f7e1
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-07-02 16:06:10 -0700

    Build basic world

--- src/giraffe.rs
+++ src/giraffe.rs
@@ -1,3 +1,6 @@
+pub const LEG_SEGMENTS:  usize = 4;
+pub const NECK_SEGMENTS: usize = 8;
+
 pub struct Giraffe {
     pub legs: Vec<u8>,
     pub neck: Vec<u8>
--- src/world.rs
+++ src/world.rs
@@ -1,18 +1,36 @@
+extern crate rand;
+
+use giraffe;
 use giraffe::Giraffe;
 
+
+const WORLD_SIZE:       u16 = 1000;
+const TREE_HEIGHT:      u32 = 48;
+const MUTATION_PERCENT: u8  = 10;
+
+
 pub struct World {
-    giraffes:    Vec<Giraffe>,
-    tree_height: u32
+    giraffes:         Vec<Giraffe>,
+    mutation_percent: u8,
+    tree_height:      u32
 }
 
 pub fn build_world() -> World {
-    let giraffes = vec![
-        Giraffe { legs: vec![1,2,3,4], neck: vec![3,4] }
-    ];
+    let giraffes: Vec<Giraffe> = (0..WORLD_SIZE).map(|_| {
+        let legs: Vec<u8> = (0..giraffe::LEG_SEGMENTS).map(|_| {
+            rand::random::<u8>()
+        }).collect();
+
+        let neck: Vec<u8> = (0..giraffe::NECK_SEGMENTS).map(|_| {
+            rand::random::<u8>()
+        }).collect();
+
+        Giraffe { legs: legs, neck: neck }
+    }).collect();
 
     World {
-        giraffes: giraffes,
-        tree_height: 48
-
+        giraffes:         giraffes,
+        mutation_percent: MUTATION_PERCENT,
+        tree_height:      TREE_HEIGHT
     }
 }
