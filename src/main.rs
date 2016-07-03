a3298a275b3462337ed901b38ee939540c1d316f
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-07-02 19:50:51 -0700

    Change fitness calculation

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,41 @@
+extern crate statistical;
+
 mod giraffe;
 mod simulation;
 mod world;
 
+
 const SIMULATION_LENGTH: u32 = 1000;
+
+
+fn print_state(world: &world::World) {
+    let mut total: i32 = 0;
+
+    let sizes = world.giraffes.iter().map(|giraffe| {
+        let mut total = 0;
+
+        for leg in giraffe.legs.iter() {
+            total = total + (*leg as i32);
+        }
+
+        for neck_segment in giraffe.neck.iter() {
+            total = total + (*neck_segment as i32);
+        }
+
+        total as f32
+    }).collect::<Vec<f32>>();
+
+    let mean   = statistical::mean(&sizes);
+    let stddev = statistical::standard_deviation(&sizes, None);
+    println!("mean: {}, std: {}", mean, stddev);
+}
 
 
 fn main () {
     let mut world = simulation::build_initial_world();
 
     for _ in 0..SIMULATION_LENGTH {
+        print_state(&world);
         world = simulation::evolve_world(world);
     }
 }
--- src/simulation.rs
+++ src/simulation.rs
@@ -5,8 +5,8 @@ use giraffe::Giraffe;
 use world::World;
 
 
-const WORLD_SIZE:       u16 = 1000;
-const TREE_HEIGHT:      u32 = 48;
+const WORLD_SIZE:       u16 = 10;
+const TREE_HEIGHT:      u32 = !0 / 4;
 const MUTATION_PERCENT: u8  = 10;
 
 pub fn build_initial_world() -> World {
@@ -30,9 +30,10 @@ pub fn build_initial_world() -> World {
 }
 
 pub fn evolve_world(world: World) -> World {
-    let fitnesses: Vec<f32> = world.giraffes.iter().map(|giraffe| {
-        calculate_fitness(&world, &giraffe)
-    }).collect();
+    let fitnesses: Vec<f32> = calculate_fitnesses(
+        &world,
+        &world.giraffes
+    );
 
     let (cumulative_densities, total_density) =
         generate_cumulative_densities(fitnesses);
@@ -72,18 +73,33 @@ fn generate_cumulative_densities(fitnesses: Vec<f32>) -> (Vec<f32>, f32) {
     (cds, total)
 }
 
-fn calculate_fitness(world: &World, giraffe: &Giraffe) -> f32 {
-    let mut total_height: u32 = 0;
+fn calculate_fitnesses(world: &World, giraffes: &Vec<Giraffe>) -> Vec<f32> {
+    let height_deltas: Vec<i32> = giraffes.iter().map(|giraffe| {
+        calculate_tree_delta(&world, &giraffe)
+    }).collect();
+
+    let max_delta = match height_deltas.iter().max() {
+        Some(m) => *m,
+        _       => 0
+    };
+
+    height_deltas.iter().map(|delta| {
+        (max_delta - *delta) as f32
+    }).collect::<Vec<f32>>()
+}
+
+fn calculate_tree_delta(world: &World, giraffe: &Giraffe) -> i32 {
+    let mut total_height: i32 = 0;
 
     for l1 in giraffe.legs.iter() {
-        total_height = total_height + (*l1 as u32);
+        total_height = total_height + (*l1 as i32);
     }
 
     for l2 in giraffe.legs.iter() {
-        total_height = total_height + (*l2 as u32);
+        total_height = total_height + (*l2 as i32);
     }
 
-    (world.tree_height as f32 - total_height as f32).abs()
+    (world.tree_height as i32 - total_height as i32).abs()
 }
 
 fn select_giraffe<'a>(cumulative_densities: &Vec<f32>, total_density: f32, giraffes: &'a Vec<Giraffe>) -> &'a Giraffe {
@@ -133,10 +149,10 @@ fn mate_giraffes(world: &World, giraffe1: &Giraffe, giraffe2: &Giraffe) -> Giraf
 
 fn apply_mutations(world: &World, characteristics: &Vec<u8>) -> Vec<u8> {
     characteristics.iter().map(|i| {
-        if random_proportion() * 100.0 > world.mutation_percent as f32 {
-            *i
-        } else {
+        if random_proportion() * 100.0 <= world.mutation_percent as f32 {
             rand::random::<u8>()
+        } else {
+            *i
         }
     }).collect()
 }
@@ -150,5 +166,6 @@ fn blend_characteristics(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
 }
 
 fn random_proportion() -> f32 {
-    (rand::random::<u8>() as f32) / ((!0 as u8) as f32)
+    let result = (rand::random::<u8>() as f32) / ((!0 as u8) as f32);
+    result
 }
