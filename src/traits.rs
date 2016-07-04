b1ab307eeae8312221aa40c23dddf7fb72a55980
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-07-04 15:43:52 -0700

    Set up trait inheritence

--- src/traits.rs
+++ src/traits.rs
@@ -1,9 +1,9 @@
-pub trait CanMate {
-    fn mate(a: &Self, b: &Self, mutation_rate: f32) -> Self;
-}
-
 pub trait CanMutate {
     fn mutate(&self, mutation_rate: f32) -> Self;
+}
+
+pub trait CanMate : CanMutate {
+    fn mate(a: &Self, b: &Self, mutation_rate: f32) -> Self;
 }
 
 pub trait HasHeight {
