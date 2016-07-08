46d9667f91968721ec1fbd003021facddb4899ed
Chris Wilhelm <chris.wilhelm@gmail.com>
2016-07-07 17:31:13 -0700

    implemented mean and one point crossover blending strategies

--- src/blending.rs
+++ src/blending.rs
@@ -3,13 +3,40 @@ use giraffe_lib::random_proportion;
 #[derive(Copy, Clone)]
 pub enum BlendingMode {
     Mean,
-    UniformCrossOver
+    OnePointCrossover,
+    UniformCrossover
 }
 
 pub fn blend_chromosomes(a: &Vec<u8>, b: &Vec<u8>, mode: BlendingMode) -> Vec<u8> {
     match mode {
-        _ => uniform_crossover(&a, &b)
+        BlendingMode::Mean              => mean(&a, &b),
+        BlendingMode::OnePointCrossover => one_point_crossover(&a, &b),
+        BlendingMode::UniformCrossover  => uniform_crossover(&a, &b)
     }
+}
+
+fn mean(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
+    a.iter().zip(b).map(|(a_gene, b_gene)| {
+        ((*a_gene as u16 + *b_gene as u16) / 2) as u8
+    }).collect::<Vec<u8>>()
+}
+
+fn one_point_crossover(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
+    let crossover_point = (random_proportion() * (a.len() as f32)) as usize;
+
+    let mut new_chromosome = vec![];
+
+    for i in 0..a.len() {
+        let source = if i < crossover_point {
+            a
+        } else {
+            b
+        };
+
+        new_chromosome.push(source[i]);
+    }
+
+    new_chromosome
 }
 
 fn uniform_crossover(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
